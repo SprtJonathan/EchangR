310e963f47a3baf90eab267c0befe30977d67182
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2023-04-20 14:10:30 +0200

    Update test-db-connection.js

--- pages/api/test-db-connection.js
+++ pages/api/test-db-connection.js
@@ -1,7 +1,7 @@
 import connection from "../../db";
 
 export default async function testDbConnection(req, res) {
-  console.log("Début de la fonction testDbConnection"); // Ajout d'un log
+  console.log("Début de la fonction testDbConnection");
 
   if (req.method !== "GET") {
     res.status(405).json({ message: "Méthode non autorisée." });
@@ -9,16 +9,16 @@ export default async function testDbConnection(req, res) {
   }
 
   try {
-    console.log("Tentative de connexion à la base de données"); // Ajout d'un log
+    console.log("Tentative de connexion à la base de données");
     const result = await connection.query("SELECT NOW()");
     if (result.rows && result.rows.length > 0) {
-      console.log("Connexion à la base de données réussie"); // Ajout d'un log
+      console.log("Connexion à la base de données réussie");
       res.status(200).json({
         message: "Connexion à la base de données réussie!",
         data: result.rows[0],
       });
     } else {
-      console.log("Erreur lors de la connexion à la base de données"); // Ajout d'un log
+      console.log("Erreur lors de la connexion à la base de données");
       res
         .status(500)
         .json({ message: "Erreur lors de la connexion à la base de données." });
@@ -27,6 +27,9 @@ export default async function testDbConnection(req, res) {
     console.error("Erreur lors de la connexion à la base de données: ", error);
     res
       .status(500)
-      .json({ message: "Erreur lors de la connexion à la base de données." });
+      .json({
+        message: "Erreur lors de la connexion à la base de données.",
+        error: error.message,
+      }); // Ajout de l'erreur dans la réponse
   }
 }
