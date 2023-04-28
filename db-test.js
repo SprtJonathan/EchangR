ca2e4262a065f32410a80957836f8706399e6cbc
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2023-04-28 14:18:47 +0200

    Create db-test.js

---
+++ db-test.js
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === "true",
+  },
+});
+
+async function testDb() {
+  await client.connect();
+
+  const res = await client.query("SELECT NOW()");
+  console.log(res.rows[0]);
+
+  await client.end();
+}
+
+testDb();
